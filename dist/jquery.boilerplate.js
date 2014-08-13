166bae0d36feab05c743c498df116d1de40b758c
Brian Hanson <brian@brianhanson.net>
2014-08-12 20:47:34 -0500

    initial commit

---
+++ dist/jquery.boilerplate.js
@@ -0,0 +1,90 @@
+/*
+ *  jQuery Boilerplate - v3.3.4
+ *  A jump-start for jQuery plugins development.
+ *  http://jqueryboilerplate.com
+ *
+ *  Made by Zeno Rocha
+ *  Under MIT License
+ */
+(function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  (function($, window, document) {
+    var Plugin, defaults, pluginName;
+    pluginName = "bigHead";
+    defaults = {
+      minHeight: 300,
+      maxHeight: 960,
+      maxWidth: 1000,
+      heightGap: 40,
+      ratio: 16 / 9,
+      paddingDirection: 'top'
+    };
+    Plugin = (function() {
+      function Plugin(element, options) {
+        this.element = element;
+        this.setRatio = __bind(this.setRatio, this);
+        this.testHeight = __bind(this.testHeight, this);
+        this.isWithinRange = __bind(this.isWithinRange, this);
+        this.sizeHead = __bind(this.sizeHead, this);
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this.init();
+      }
+
+      Plugin.prototype.init = function() {
+        this.window = $(window);
+        this.$el = $(this.element);
+        this.windowHeight = this.window.height() - this.settings.heightGap;
+        return this.sizeHead(this.windowHeight);
+      };
+
+      Plugin.prototype.sizeHead = function(height) {
+        if (this.isWithinRange(height)) {
+          this.$el.css("padding-bottom", 0);
+          this.$el.outerHeight(this.windowHeight);
+          return this.setRatio();
+        }
+      };
+
+      Plugin.prototype.isWithinRange = function(height) {
+        var heightAtMax, windowToHeightRatio;
+        windowToHeightRatio = height / this.window.width();
+        if (!this.testHeight(height)) {
+          return false;
+        }
+        heightAtMax = this.settings.maxWidth * windowToHeightRatio;
+        if (!this.testHeight(heightAtMax)) {
+          return false;
+        }
+        return true;
+      };
+
+      Plugin.prototype.testHeight = function(height) {
+        return height > this.settings.minHeight && height < this.settings.maxHeight;
+      };
+
+      Plugin.prototype.setRatio = function() {
+        var h, ratio, w;
+        w = this.$el.outerWidth();
+        h = this.$el.outerHeight();
+        ratio = (h / w) * 100;
+        return this.$el.css({
+          "padding-bottom": "" + ratio + "%",
+          "height": "auto"
+        });
+      };
+
+      return Plugin;
+
+    })();
+    return $.fn[pluginName] = function(options) {
+      return this.each(function() {
+        if (!$.data(this, "plugin_" + pluginName)) {
+          return $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+        }
+      });
+    };
+  })(jQuery, window, document);
+
+}).call(this);
---
+++ src/jquery.boilerplate.coffee
@@ -0,0 +1,82 @@
+# Note that when compiling with coffeescript, the plugin is wrapped in another
+# anonymous function. We do not need to pass in undefined as well, since
+# coffeescript uses (void 0) instead.
+do ($ = jQuery, window, document) ->
+
+	# window and document are passed through as local variable rather than global
+	# as this (slightly) quickens the resolution process and can be more efficiently
+	# minified (especially when both are regularly referenced in your plugin).
+
+	# Create the defaults once
+	pluginName = "bigHead"
+	defaults =
+		minHeight: 300
+		maxHeight: 960
+		maxWidth: 1000
+		heightGap: 40
+		ratio: 16/9
+		paddingDirection: 'top'
+
+	# The actual plugin constructor
+	class Plugin
+		constructor: (@element, options) ->
+			# jQuery has an extend method which merges the contents of two or
+			# more objects, storing the result in the first object. The first object
+			# is generally empty as we don't want to alter the default options for
+			# future instances of the plugin
+			@settings = $.extend {}, defaults, options
+			@_defaults = defaults
+			@init()
+
+		init: ->
+			# Place initialization logic here
+			# You already have access to the DOM element and the options via the instance,
+			# e.g., @element and @settings
+			@window = $(window)
+			@$el = $(@element)
+			@windowHeight = @window.height() - @settings.heightGap
+
+			@sizeHead(@windowHeight)
+
+		sizeHead: (height) =>
+			# Only resize if the window height is within the parameters
+	    if @isWithinRange(height)
+	      # Zero out the intrinsic ratio padding
+	      @$el.css("padding-bottom", 0)
+	      # Set the $el height to specified size
+	      @$el.outerHeight(@windowHeight)
+
+	      @setRatio()
+
+		isWithinRange: (height) =>
+			windowToHeightRatio = height / @window.width()
+
+			#First, we make sure that the existing height isn't out of range
+			return false unless @testHeight(height)
+
+			#Then we make sure the height will be within range at the maximum window width
+			heightAtMax = @settings.maxWidth * windowToHeightRatio
+			return false unless @testHeight(heightAtMax)
+
+			#Final Result
+			return true
+
+		testHeight: (height) =>
+			height > @settings.minHeight and height < @settings.maxHeight
+
+		setRatio: =>
+			#Ensures that on further resizes the header scales proportionally
+			w = @$el.outerWidth()
+			h = @$el.outerHeight()
+			ratio = (h / w) * 100
+			@$el.css({
+				"padding-bottom" : "#{ratio}%"
+				"height" : "auto"
+			})
+
+	# A really lightweight plugin wrapper around the constructor,
+	# preventing against multiple instantiations
+	$.fn[pluginName] = (options) ->
+		@each ->
+			unless $.data @, "plugin_#{pluginName}"
+				$.data @, "plugin_#{pluginName}", new Plugin @, options
